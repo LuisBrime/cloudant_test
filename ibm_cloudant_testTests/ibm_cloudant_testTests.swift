cd537672549b967675a6ad46b630a8caf2b6a510
Luis Brime <brimegmz@gmail.com>
2018-11-09 11:02:55 -0600

    Initial Commit

---
+++ ibm_cloudant_testTests/ibm_cloudant_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  ibm_cloudant_testTests.swift
+//  ibm_cloudant_testTests
+//
+//  Created by Luis Eduardo Brime Gomez on 11/9/18.
+//  Copyright © 2018 Luis Eduardo Brime Gomez. All rights reserved.
+//
+
+import XCTest
+@testable import ibm_cloudant_test
+
+class ibm_cloudant_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
