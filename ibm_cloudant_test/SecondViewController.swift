f4062b640a0b6a6696b93a6d89776a2afee9b3f8
Luis Brime <brimegmz@gmail.com>
2018-11-16 09:53:47 -0600

    connection works

--- ibm_cloudant_test/SecondViewController.swift
+++ ibm_cloudant_test/SecondViewController.swift
@@ -23,35 +23,45 @@ class SecondViewController: UIViewController {
     var age : String = ""
     var email : String = ""
     
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        DispatchQueue.main.async {
-            print("Retrieving data...")
-            let cloudantURL = NSURL(string: self.url)
-            let client = CouchDBClient(url: cloudantURL! as URL, username: self.username, password: self.password)
-            let dbName = "test"
-            
-            let read = GetDocumentOperation(id: "ba290984d2dcbeca3cb8cc22ec5c335c", databaseName: dbName) { (response, httpInfo, error) in
-                if let error = error {
-                    print("ERROR \(error)")
-                } else {
-                    print("Data retrieved...")
-                    self.name = response?["name"] as! String
-                    self.age  = response?["age"] as! String
-                    self.email = response?["email"] as! String
-                    
-                    self.nLabel.text = self.name
-                    self.aLabel.text = self.age
-                    self.eLabel.text = self.email
+        
+        loadData()
+    }
+    
+    func loadData() {
+        self.indicator.startAnimating()
+        
+        DispatchQueue.global().async {
+            DispatchQueue.main.async {
+                print("Retrieving data...")
+                let cloudantURL = NSURL(string: self.url)
+                let client = CouchDBClient(url: cloudantURL! as URL, username: self.username, password: self.password)
+                let dbName = "test"
+                
+                let read = GetDocumentOperation(id: "ba290984d2dcbeca3cb8cc22ec5c335c", databaseName: dbName) { (response, httpInfo, error) in
+                    if let error = error {
+                        print("ERROR \(error)")
+                    } else {
+                        print("Data retrieved...")
+                        self.name = response?["name"] as! String
+                        self.age  = response?["age"] as! String
+                        self.email = response?["email"] as! String
+                        
+                        self.nLabel.text = self.name
+                        self.aLabel.text = self.age
+                        self.eLabel.text = self.email
+                        
+                        self.indicator.stopAnimating()
+                    }
                 }
+                client.add(operation: read)
+                print("Done...")
             }
-            client.add(operation: read)
-            print("Done...")
-            
         }
-        
-        
     }
 
 
